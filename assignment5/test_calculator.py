85cc4af4f7e0c6ecab0637770a5394a535f9dde3
JIHANRJ <131355046+JIHANRJ@users.noreply.github.com>
2023-10-29 23:15:03 +0530

    Update test_calculator.py

--- assignment5/test_calculator.py
+++ assignment5/test_calculator.py
@@ -7,16 +7,16 @@ class CalculatorTest(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(10, self.calc.add(-3, 7), "The addition is wrong")
+        self.assertEqual(10, self.calc.add(5, 5), "The addition is wrong")
 
     def test_subtract(self):
-        self.assertEqual(12, self.calc.subtract(0, 3), "Subtraction is wrong")
+        self.assertEqual(2, self.calc.subtract(4, 2), "Subtraction is wrong")
 
     def test_multiply(self):
-        self.assertEqual(30, self.calc.multiply(-5, 6), "Multiplication is wrong")
+        self.assertEqual(30, self.calc.multiply(5, 6), "Multiplication is wrong")
 
     def test_divide(self):
-        self.assertEqual(3, self.calc.divide(6, 0), "Division is wrong")
+        self.assertEqual(3, self.calc.divide(6, 3), "Division is wrong")
 
 if __name__ == '__main__':
     unittest.main()
